9b8776123a4844e56b04386c1cb5c2cdd4c134e3
chenjond <113214009+chenjond@users.noreply.github.com>
2022-09-19 15:36:56 -0400

    Add comment on print

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,3 @@
 #!/bin/python3
-
-print("Hello World\nI have been at U of T for 4 years (including PEY)")
+# This is very descriptive.
+print("Hello World\nI have been at U of T for 4 years (including PEY).")
